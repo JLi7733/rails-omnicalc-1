de351ac9df97c3b265fe47384c58877ecb8af93f
JLi7733 <48297521+JLi7733@users.noreply.github.com>
2025-02-11 15:56:41 -0600

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,17 @@
 Rails.application.routes.draw do
+
+  get("/", {:controller =>"calc", :action => "home"})
+
+  get(""/square/new"", {:controller =>"calc", :action => "square"})
+  get(""/square/results"", {:controller =>"calc", :action => "square_results"})
+  
+  get(""/square_root/new"", {:controller =>"calc", :action => "sqrt"})
+  get(""/square_root/results"", {:controller =>"calc", :action => "sqrt_results"})
+  
+  get(""/random/new"", {:controller =>"calc", :action => "random"})
+  get(""/random/results"", {:controller =>"calc", :action => "random_results"})
+  
+  get(""/payment/new"", {:controller =>"calc", :action => "payment"})
+  get(""/payment/results"", {:controller =>"calc", :action => "payment_results"})
   
 end
